15766fa5acbbc7781efe363085272ebf42299afa
Sadler Jerome <jerome@sadler-jerome.fr>
2020-06-25 15:08:25 +0200

    Fix typo

--- includes/admin.php
+++ includes/admin.php
@@ -38,7 +38,7 @@ function wpt_tester_admin_page_output() {
     <p><?php esc_html_e( 'Test Slack team notifications', 'wp-translations-tester' ); ?></p>
     <p><?php esc_html_e( 'Test Slack TM notifications', 'wp-translations-tester' ); ?></p>
 
-    <p><?php esc_html_e( 'Testing WPT App’s notifications', 'wp-translations-tester' ); ?></p>
+    <p><?php esc_html_e( 'Testing WPT App notifications', 'wp-translations-tester' ); ?></p>
     <p><?php esc_html_e( 'Testing second textdomain', 'wp-translations-tester-bis' ); ?></p>
   </div>
   <?php
