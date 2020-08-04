9d23cb17706ab8f2b5929ee2ca6005e832ead4d1
G3ronim0 <jerome@sadler-jerome.fr>
2020-08-04 18:03:52 +0200

    Update string

--- plugin1/admin.php
+++ plugin1/admin.php
@@ -8,7 +8,7 @@ add_action('admin_menu', 'wpt_tester_admin_menu');
 function wpt_tester_admin_page_plugin1_output() {
   ?>
   <div class="wrap">
-		<h2><?php _e( 'Plugin 1 Options', 'wp-translations-tester-bis' ); ?></h2>
+		<h2><?php _e( 'Plugin Options', 'wp-translations-tester-bis' ); ?></h2>
 
 	  <p><?php esc_html_e( 'Second commit on GitHub', 'wp-translations-tester-bis' ); ?></p>
     <p><?php esc_html_e( 'Testing second textdomain', 'wp-translations-tester-bis' ); ?></p>
