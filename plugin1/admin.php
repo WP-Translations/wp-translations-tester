b5dc7003d5b2cf34773c1a18e9ee3202bc451dc0
G3ronim0 <jerome@sadler-jerome.fr>
2020-08-04 12:45:14 +0200

    Modify to match mono repo structure

---
+++ plugin1/admin.php
@@ -0,0 +1,17 @@
+<?php
+
+function wpt_tester_admin_menu_plugin1() {
+	add_plugins_page( 'WPT Tester Plugin 1', 'WPT Tester Plugin 1', 'manage_options', 'wpt-tester-admin-page-plugin1', 'wpt_tester_admin_page_plugin1_output' );
+}
+add_action('admin_menu', 'wpt_tester_admin_menu');
+
+function wpt_tester_admin_page_plugin1_output() {
+  ?>
+  <div class="wrap">
+		<h2><?php _e( 'Plugin 1 Options', 'wp-translations-tester-bis' ); ?></h2>
+
+
+    <p><?php esc_html_e( 'Testing second textdomain', 'wp-translations-tester-bis' ); ?></p>
+  </div>
+  <?php
+}
