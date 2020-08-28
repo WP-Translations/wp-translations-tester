dbfb98781ce0715b5e2559847bd33fbeeb6b1991
G3ronim0 <jerome@sadler-jerome.fr>
2020-08-28 16:17:06 +0200

    Update string

--- plugin-1/admin.php
+++ plugin-1/admin.php
@@ -13,7 +13,7 @@ function wpt_tester_admin_page_plugin1_output() {
 	  <p><?php esc_html_e( 'Second commit on GitHub', 'wp-translations-tester-bis' ); ?></p>
     <p><?php esc_html_e( 'Testing second textdomain', 'wp-translations-tester-bis' ); ?></p>
     <p><?php esc_html_e( 'New string', 'wp-translations-tester-bis' ); ?></p>
-    <p><?php esc_html_e( 'Hello world!', 'wp-translations-tester-bis' ); ?></p>
+    <p><?php esc_html_e( 'Hello world', 'wp-translations-tester-bis' ); ?></p>
     <p><?php esc_html_e( 'Ok for mono repo!', 'wp-translations-tester-bis' ); ?></p>
   </div>
   <?php
