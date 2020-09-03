4f0bb49c559f19b4d8fe9be7baf6d35aeb40f7d5
G3ronim0 <jerome@sadler-jerome.fr>
2020-09-03 11:27:40 +0200

    Introduice t15s updater

--- wp-translations-tester.php
+++ wp-translations-tester.php
@@ -12,7 +12,19 @@
  * @package WPTT
  * @category Core
  * @author Sadler Jerome
- * @version 1.0.3
+ * @version 1.0.0
  */
 
 require_once( dirname(__FILE__) .'/wp-translations-tester/admin.php' );
+
+function wp_translations_tester_init_t15s() {
+    if ( class_exists( 'WPT_Tester_Language_Packs' ) ) {
+        $t15s_updater = new WPT_Tester_Language_Packs(
+            'plugin',
+            'wp-translations-tester',
+            'https://packages.translationspress.com/wp-translations/wp-translations-tester/packages.json'
+        );
+        $project = $t15s_updater->add_project();
+    }
+}
+add_action( 'init', 'wp_translations_tester_init_t15s' );
