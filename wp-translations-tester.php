6b7c5083ea548424223a7d7740f3c8781116925b
G3ronim0 <jerome@sadler-jerome.fr>
2020-09-03 13:55:17 +0200

    Update T15S Updater

--- wp-translations-tester.php
+++ wp-translations-tester.php
@@ -19,13 +19,11 @@ require_once( dirname(__FILE__) .'/inc/translations-updater.php' );
 require_once( dirname(__FILE__) .'/wp-translations-tester/admin.php' );
 
 function wp_translations_tester_init_t15s() {
-    if ( class_exists( 'WPT_Tester_Language_Packs' ) ) {
-        $t15s_updater = new WPT_Tester_Language_Packs(
-            'plugin',
+    if ( class_exists( 'WP_Translations_Tester_T15S' ) ) {
+        $t15s_updater = new WP_Translations_Tester_T15S(
             'wp-translations-tester',
             'https://packages.translationspress.com/wp-translations/wp-translations-tester/packages.json'
         );
-        $project = $t15s_updater->add_project();
     }
 }
 add_action( 'init', 'wp_translations_tester_init_t15s' );
