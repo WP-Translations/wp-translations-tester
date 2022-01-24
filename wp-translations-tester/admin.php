8e76d58d4d10e3d188d0d618cc2ab40c5936204e
G3ronim0 <jerome@sadler-jerome.fr>
2022-01-24 11:05:28 +0100

    bye

--- wp-translations-tester/admin.php
+++ wp-translations-tester/admin.php
@@ -20,7 +20,7 @@ function wpt_tester_admin_page_output() {
     <p><?php esc_html_e( 'Dashboard', 'wp-translations-tester' ); ?></p>
     <p><?php esc_html_e( 'Visibility', 'wp-translations-tester' ); ?></p>
     <p><?php esc_html_e( 'Padding', 'wp-translations-tester' ); ?></p>
-    <p><?php esc_html_e( 'Post', 'wp-translations-tester' ); ?></p>
+
 		<p><?php esc_html_e( 'WebHook testing', 'wp-translations-tester' ); ?></p>
 		<p><?php esc_html_e( 'WebHook trigger', 'wp-translations-tester' ); ?></p>
 		<p><?php esc_html_e( 'Propagation', 'wp-translations-tester' ); ?></p>
