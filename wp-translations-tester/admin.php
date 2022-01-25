93a3946636f0db5570450fa904dab4d0f5432f36
G3ronim0 <jerome@sadler-jerome.fr>
2022-01-25 09:56:35 +0100

    new s

--- wp-translations-tester/admin.php
+++ wp-translations-tester/admin.php
@@ -30,6 +30,7 @@ function wpt_tester_admin_page_output() {
 		<p><?php esc_html_e( 'WebHook testing', 'wp-translations-tester' ); ?></p>
 		<p><?php esc_html_e( 'WebHook trigger', 'wp-translations-tester' ); ?></p>
 		<p><?php esc_html_e( 'Propagation', 'wp-translations-tester' ); ?></p>
+		<p><?php esc_html_e( 'Async task', 'wp-translations-tester' ); ?></p>
 
 
   </div>
