437f7482f46e270ed82da417cabee569a9ea1c1a
G3ronim0 <jerome@sadler-jerome.fr>
2022-01-25 11:18:33 +0100

    new

--- wp-translations-tester/admin.php
+++ wp-translations-tester/admin.php
@@ -38,6 +38,7 @@ function wpt_tester_admin_page_output() {
 		<p><?php esc_html_e( 'Add it by clicking on the blacklist.', 'wp-translations-tester' ); ?></p>
 		<p><?php esc_html_e( 'Add Keyword', 'wp-translations-tester' ); ?></p>
 		<p><?php esc_html_e( 'Blockquotes', 'wp-translations-tester' ); ?></p>
+		<p><?php esc_html_e( 'Content', 'wp-translations-tester' ); ?></p>
 
 
   </div>
