2493295682bc31f45cea604a398846f5f52d48ed
G3ronim0 <jerome@sadler-jerome.fr>
2022-01-26 02:15:34 +0100

    new

--- wp-translations-tester/admin.php
+++ wp-translations-tester/admin.php
@@ -47,6 +47,7 @@ function wpt_tester_admin_page_output() {
 		<p><?php esc_html_e( 'Job', 'wp-translations-tester' ); ?></p>
 		<p><?php esc_html_e( 'Plane', 'wp-translations-tester' ); ?></p>
 		<p><?php esc_html_e( 'Words', 'wp-translations-tester' ); ?></p>
+		<p><?php esc_html_e( 'Menu', 'wp-translations-tester' ); ?></p>
 
 
   </div>
