4f54386019d3baf712a29d15caf4789433b5d745
Sadler Jerome <jerome@sadler-jerome.fr>
2022-12-14 16:36:25 +0100

    Update admin.php

--- wp-translations-tester/admin.php
+++ wp-translations-tester/admin.php
@@ -32,6 +32,7 @@ function wpt_tester_admin_page_output() {
     <p><?php esc_html_e( 'Token', 'wp-translations-tester' ); ?></p>
     <p><?php esc_html_e( 'Originals', 'wp-translations-tester' ); ?></p>
 	  <p><?php esc_html_e( 'Translations', 'wp-translations-tester' ); ?></p>
+	  <p><?php esc_html_e( 'Margin', 'wp-translations-tester' ); ?></p>
    <p><?php esc_html_e(
 					sprintf(
 						/* translators: %d is a number of strings translations */
