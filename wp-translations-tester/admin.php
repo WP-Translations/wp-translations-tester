fcdb2c2de27a60396a989504286eadc826cf88c1
G3ronim0 <jerome@sadler-jerome.fr>
2023-03-21 18:20:40 +0100

    New strings

--- wp-translations-tester/admin.php
+++ wp-translations-tester/admin.php
@@ -31,8 +31,11 @@ function wpt_tester_admin_page_output() {
     <p><?php esc_html_e( 'i18n', 'wp-translations-tester' ); ?></p>
     <p><?php esc_html_e( 'Token', 'wp-translations-tester' ); ?></p>
     <p><?php esc_html_e( 'Originals', 'wp-translations-tester' ); ?></p>
-	  <p><?php esc_html_e( 'Translations', 'wp-translations-tester' ); ?></p>
-	  <p><?php esc_html_e( 'Margin', 'wp-translations-tester' ); ?></p>
+    <p><?php esc_html_e( 'Originals', 'wp-translations-tester' ); ?></p>
+	  <p><?php esc_html_e( 'Activated', 'wp-translations-tester' ); ?></p>
+	  <p><?php esc_html_e( 'Deactivated', 'wp-translations-tester' ); ?></p>
+	   <p><?php esc_html_e( 'Invalid', 'wp-translations-tester' ); ?></p>
+	   <p><?php esc_html_e( 'Updated', 'wp-translations-tester' ); ?></p>
    <p><?php esc_html_e(
 					sprintf(
 						/* translators: %d is a number of strings translations */
